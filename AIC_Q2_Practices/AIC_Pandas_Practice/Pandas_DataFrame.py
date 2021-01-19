358e3a6f2c12e57945f8732f6ac40a5c9874210b
Flower <pinktulip212@gmail.com>
2021-01-19 22:01:18 +0500

    NP Practice Preparing Numpy Quiz

---
+++ AIC_Q2_Practices/AIC_Pandas_Practice/Pandas_DataFrame.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import time as tm
+from AIC_Q2_Practices import ANSIColors as clr
+
+print(clr.COLORS['default'])
+"""
+What is DataFrame?
+A DataFrame is a rectangular table a data and contains an ordered collection of columns, each of which can be different value type. The DataFrame has both row ans columsn index; it can be thought of a dictionary of Series all sharing the same index.
+>> Conscept:
+While a DataFrame is physically two-dimensional, you can use it to
+represent higher dimensional data in a tabular format using hier‐
+archical indexing.
+
+There are many ways to construct a DataFrame, though one of the most common is
+from a dict of *equal-length* lists or NumPy arrays:
+"""
+# Main execution time starts from here
+start_time = tm.time()  # returns the current time in milliseconds
+
+# This Diction contains three keys with list of same size as values
+# if we create a Pandas Series from this; there will be three indexes
+#  If this dictionary fed to Pandas Dataframe then; there will be three
+# Series stacked horizontally in DataFrame
+py_dict_states = {'State': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada', 'Nevada'],
+                  'Year': [2000, 2001, 2002, 2000, 2001, 2003], 'Population': [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]}
+print("\u001b[1m\u001b[32m")
+ps_dict_states = pd.Series(py_dict_states,name='State Population Summary')
+print("Pandas Series from Dictionary:",ps_dict_states)
+print("\u001b[0m")
+print('*' * 10,"DataFrame",'*'*10)
+pdf_states_pop = pd.DataFrame(py_dict_states)
+print('First DataFrame:',pdf_states_pop,sep='\n')
+pdf_rearranged_cols = pd.DataFrame(ps_dict_states)
+print('Cols Re-organized DataFrame:',pdf_rearranged_cols,sep='\n')
+
+"""This method returns the top n rows of the series or DataFrame
+    by default it returns top 5 rows
+"""
+top_row_pdf = pdf_states_pop.head(1)
+print("Head of the DF",top_row_pdf,sep='\n')
+
+# Measurement of execution time
+print('*' * 10,"Execution Time",'*'*10)
+end_time = tm.time()
+print("Start time:", start_time)
+print("End Time:", end_time)
+consumed_time = end_time - start_time
+print("Time Taken:",consumed_time)
+
+
--- AIC_Q2_Practices/AIC_Pandas_Practice/Pandas_Series_Creation.py
+++ AIC_Q2_Practices/AIC_Pandas_Practice/Pandas_Series_Creation.py
@@ -2,15 +2,43 @@ import pandas as pd
 import numpy as np
 
 # Python Data Structure
-py_tpl = tuple(range(10, 101, 10))
-py_lst = [5, 10, 15, 45, 15, np.nan, 20, 45, 50, 25]
+py_tpl = tuple(range(10, 101, 20))
+py_lst = [5, 10, 15, 45, 15, np.nan, 20]
 print("Pandas Series with Python Tuple")
 ps_with_tpl = pd.Series(py_tpl, index=range(1, len(py_tpl) + 1), dtype='i4')
 print(ps_with_tpl)
-print(ps_with_tpl.values)
+print("Series Values:", ps_with_tpl.values)
+print("Series Index:", ps_with_tpl.index)
 
 print('*' * 50)
 print("Pandas Series with Python List")
-ps_with_lst = pd.Series(py_lst, index=range(1, len(py_lst)), dtype=np.float)
+ps_with_lst = pd.Series(py_lst, index=range(1, len(py_lst) + 1), dtype=np.float)
 print(ps_with_lst)
-print("Unique Values Count\n{0}".format(ps_with_lst.value_counts(dropna=False)))
+print("Series Values:", ps_with_lst.values)
+print("Series Index:", ps_with_lst.index)
+# Altering index inplace with assignment
+# number of new indexes must be same as previous indexes
+ps_with_lst.index=['a','b','c','d','e','f','g']
+print("ps_with_lst with new indexes",ps_with_lst,sep='\n')
+# print("Unique Values Count\n{0}".format(ps_with_lst.value_counts(dropna=False)))
+
+
+print("{0}SeriesWithDictionary{0}".format('*' * 10))
+# While feeding data to pandas series with Python Dictionary
+# Keys of the dictionary becomes the index and values remain values in pandas series.
+states_with_pop = {"Lahore": (500,200,300), "Islamabad": (700,900), "Karachi": 1500, "Peshawar": 600}
+ps_with_dict = pd.Series(states_with_pop)
+print("Dict Series:", ps_with_dict, sep="\n")
+# by reindexing you can add more indexes, rearrange previous indexes
+ps_with_dict=ps_with_dict.reindex(['Lahore', 'Karachi',"Islamabad", 'Multan',"Peshawar"])
+print("After Reindexing of the Series")
+# Performs slicing of the series. Note: For indexing you have to exactly specify the index of the series.
+print("Series Slicing:", ps_with_dict[0:], sep="\n")
+# Numpy ndarray like filtering on pandas series.
+filt_sr = ps_with_lst[ps_with_lst > 30]
+print("Filterd Series:", filt_sr)
+# Create a third series by stacking two of them
+sr_concat = pd.Series(np.hstack((ps_with_tpl.values, ps_with_dict.values)), index=np.hstack((ps_with_tpl.index, ps_with_dict.index)))
+print("Concatenated Series", sr_concat, sep="\n")
+# Finds the null value if series contains
+print(ps_with_lst.notnull())
